7a17b1c30341053eb4b6ae83593cc0b4f6f71865
Santosh-Ku-Sahoo <sksahoo1401@gmail.com>
2026-06-01 14:50:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8081;
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message: "Home Page"
+    })
+})
+
+// app.all('/{*splat}',(req,res)=>{ // This will handle all the routes which are not defined above
+//     res.status(404).json({
+//         message: "Page Not Found"
+//     })
+// })
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up and running on http://localhost:${PORT}`);
+})
